0fac4c4d533057b7ef046e40ca05cb010e7c56f5
deng-xinyi <43009981+deng-xinyi@users.noreply.github.com>
2019-07-03 13:39:21 -0400

    added dijkstra for finding shortest paths on graph

--- ssm/preprocessing.py
+++ ssm/preprocessing.py
@@ -178,3 +178,28 @@ def mpp_bin(spmark, tbin_section, n_tet, n_mark, maxispk, timebin):
     spmark_bin = np.concatenate((spikes_bin, marks_bin), axis=3)
     
     return spmark_bin
+
+
+def dijkstra(G, s):
+    """
+    find all shortest paths from s to each other vertex in graph G
+    """    
+    n = len(G)
+    visited = [False]*n
+    weights = [math.inf]*n
+    path = [None]*n
+    queue = []
+    weights[s] = 0
+    hq.heappush(queue, (0, s))
+    while len(queue) > 0:
+        g, u = hq.heappop(queue)
+        visited[u] = True
+        for v, w in G[u]:
+            if not visited[v]:
+                print(v, w, g, u)
+                f = g + w
+                if f < weights[v]:
+                    weights[v] = f
+                    path[v] = u
+                    hq.heappush(queue, (f, v))
+    return path, weights
